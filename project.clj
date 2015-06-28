dc777cb5c18b9beee3fa118bc1ef8baf60b48a1d
Jon Eisen <jon@metricfeat.com>
2015-06-28 10:38:31 -0400

    add travis support

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "https://github.com/Rafflecopter/clj-qb/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.7.0-RC2"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+                   :plugins [[lein-midje "3.1.3"]]}})
