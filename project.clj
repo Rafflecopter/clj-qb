79923994b6306d3f4b5abb39b33d1f56feffff8e
Jon Eisen <jon@metricfeat.com>
2015-07-02 18:32:22 -0600

    update for deploy to clojars

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :url "https://github.com/Rafflecopter/clj-qb"
   :license {:name "MIT"
             :url "https://github.com/Rafflecopter/clj-qb/blob/master/LICENSE"}
+  :scm {:name "git"
+        :url "https://github.com/Rafflecopter/clj-qb"}
+  :deploy-repositories [["clojars" {:creds :gpg}]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
