e4116983fdd677f46788d8049e6962f34e8af4f7
Jon Eisen <jon@metricfeat.com>
2015-08-10 16:10:44 -0600

    Version 0.2.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.rafflecopter/qb "0.2.1"
+(defproject com.rafflecopter/qb "0.2.2-SNAPSHOT"
   :description "Queue interface and helpers"
   :url "https://github.com/Rafflecopter/clj-qb"
   :license {:name "MIT"
