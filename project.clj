3286d04bd2f5f558dc04146ac6ecd0d122418577
Jon Eisen <jon@metricfeat.com>
2015-08-10 16:10:14 -0600

    Fix release in project clj

--- project.clj
+++ project.clj
@@ -1,17 +1,11 @@
-(defproject com.rafflecopter/qb "0.2.0"
+(defproject com.rafflecopter/qb "0.2.1-SNAPSHOT"
   :description "Queue interface and helpers"
   :url "https://github.com/Rafflecopter/clj-qb"
   :license {:name "MIT"
             :url "https://github.com/Rafflecopter/clj-qb/blob/master/LICENSE"}
   :scm {:name "git"
         :url "https://github.com/Rafflecopter/clj-qb"}
-  :deploy-repositories [["clojars" {:creds :gpg}]]
-  :release-tasks [["vcs" "assert-committed"]
-                  ["change" "version"
-                   "leiningen.release/bump-version" "release"]
-                  ["vcs" "commit"]
-                  ["vcs" "tag"]
-                  ["deploy" "clojars"]]
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
